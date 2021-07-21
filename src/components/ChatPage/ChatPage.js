eaa887a3afac60623e583d8ae91b5d9bfc2df5d2
dhcho1034 <dhcho1034@gmail.com>
2021-07-22 02:08:48 +0900

    CRA structure and react-router-dom

---
+++ src/components/ChatPage/ChatPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ChatPage() {
+  return (
+    <div>
+      ChatPage
+      <br />
+    </div>
+  );
+}
+
+export default ChatPage;
---
+++ src/components/RegisterPage/RegisterPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RegisterPage() {
+  return (
+    <div>
+      RegisterPage
+      <br />
+    </div>
+  );
+}
+
+export default RegisterPage;

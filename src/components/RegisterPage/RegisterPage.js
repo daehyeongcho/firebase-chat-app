057e966fe57010cc38505372a6ea5c0c1bf5bba1
dhcho1034 <dhcho1034@gmail.com>
2021-07-24 17:03:23 +0900

    회원가입 페이지 UI 만들기

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,10 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function RegisterPage() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div>
-      RegisterPage
-      <br />
+    <div className="auth-wrapper">
+      <div style={{ textAlign: "center" }}>
+        <h3>Register</h3>
+      </div>
+      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <label>Email</label>
+        <input name="email" type="email" {...register("example")} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <label>Name</label>
+        <input
+          name="name"
+          {...register("exampleRequired", { required: true })}
+        />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+          {...register("exampleRequired", { required: true })}
+        />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <label>Password Confirm</label>
+        <input
+          name="passwordConfirm"
+          type="password"
+          {...register("exampleRequired", { required: true })}
+        />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+        <input type="submit" />
+        <Link style={{ color: "gray", textDecoration: "none" }} to="login">
+          이미 아이디가 있다면...
+        </Link>
+      </form>
     </div>
   );
 }

532a7e277c0155a1611ffb8f54fd9e572b2d7762
dhcho1034 <dhcho1034@gmail.com>
2021-07-31 17:19:50 +0900

    로그인 페이지 만들기

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,10 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import firebase from "../../firebase";
 
 function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [errorFromSubmit, setErrorFromSubmit] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+
+      await firebase
+        .auth()
+        .signInWithEmailAndPassword(data.email, data.password);
+
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setErrorFromSubmit(error.message);
+      setLoading(false);
+      setTimeout(() => {
+        setErrorFromSubmit("");
+      }, 3000);
+    }
+  };
+
   return (
-    <div>
-      LoginPage
-      <br />
+    <div className="auth-wrapper">
+      <div style={{ textAlign: "center" }}>
+        <h3>Login</h3>
+      </div>
+      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        {/* include validation with required or other standard HTML validation rules */}
+        <label>Email</label>
+        <input
+          name="email"
+          type="email"
+          {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {/* errors will return when field validation fails  */}
+        {errors.email && <span>This email field is required.</span>}
+
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+          {...register("password", { required: true, minLength: 8 })}
+        />
+        {errors.password && errors.password.type === "required" && (
+          <span>This password field is required.</span>
+        )}
+        {errors.password && errors.password.type === "minLength" && (
+          <span>Password must have at least 8 characters.</span>
+        )}
+
+        {errorFromSubmit && <p>{errorFromSubmit}</p>}
+
+        <input type="submit" disabled={loading} />
+        <Link style={{ color: "gray", textDecoration: "none" }} to="register">
+          아직 아이디가 없다면...
+        </Link>
+      </form>
     </div>
   );
 }
